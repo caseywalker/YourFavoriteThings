2670e9841bed4e5d592f6287bea52eb97b1e34f5
Caseywalker <casey.walker118@gmail.com>
2021-07-23 13:07:26 -0500

    Adding favorite restaurant classes.

--- YourFavoriteThings/Favorites/Restaurant.cs
+++ YourFavoriteThings/Favorites/Restaurant.cs
@@ -8,5 +8,26 @@ namespace YourFavoriteThings.Favorites
 {
   class Restaurant
   {
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string Type { get; set; }
+    public string Number { get; set; }
+
+    public Restaurant(string name, string location, string type)
+    {
+      Name = name;
+      Location = location;
+      Type = type;
+    }
+
+    public void Info()
+    {
+      Console.WriteLine($"Restaurant {Name} is located in {Location}, and is a {Type} restaurant.");
+    }
+
+    public void Order()
+    {
+      Console.WriteLine($"To order food from {Name}, call {Number}.");
+    }
   }
 }
--- YourFavoriteThings/Program.cs
+++ YourFavoriteThings/Program.cs
@@ -13,8 +13,29 @@ namespace YourFavoriteThings
       pizza.Name = "Pizza";
       pizza.Meal = "Dinner";
 
+      var tacos = new Food
+      {
+        Name = "Tacos",
+        Meal = "Lunch"
+      };
+
+      tacos.Cook();
+      tacos.Eat();
+
       pizza.Cook();
       pizza.Eat();
+
+      var salvos = new Restaurant("Salvo's", "Smyrna", "Pizza");
+      salvos.Number = "555-5555";
+
+      var hemingways = new Restaurant("Hemingway's", "Nashville", "Tavern");
+      hemingways.Number = "867-5309";
+
+      salvos.Info();
+      salvos.Order();
+
+      hemingways.Info();
+      hemingways.Order();
 
     }
   }
