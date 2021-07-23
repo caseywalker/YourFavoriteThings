668a8ee0291b1aa1839d2c199ef8b9a59e873525
Caseywalker <casey.walker118@gmail.com>
2021-07-23 12:44:57 -0500

    Add project files.

---
+++ YourFavoriteThings/Favorites/Food.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourFavoriteThings.Favorites
+{
+  class Food
+  {
+    public string Name { get; set; }
+    public string Meal { get; set; }
+
+    public void Eat()
+    {
+      Console.WriteLine($"You just ate {Name} for {Meal}.");
+    }
+
+    public void Cook()
+    {
+      Console.WriteLine($"You just cooked {Name} for {Meal}.");
+    }
+
+  }
+}
---
+++ YourFavoriteThings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using YourFavoriteThings.Favorites;
+
+namespace YourFavoriteThings
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Your favorite things");
+
+      var pizza = new Food();
+      pizza.Name = "Pizza";
+      pizza.Meal = "Dinner";
+
+      pizza.Cook();
+      pizza.Eat();
+
+    }
+  }
+}
